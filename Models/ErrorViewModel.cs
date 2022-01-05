baac79e11500a7d86dfa2b1fea94d89585ba556f
LogeshKumar762 <kumarlogesh762@gmail.com>
2022-01-05 23:52:05 +0530

    newly added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewTestApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
